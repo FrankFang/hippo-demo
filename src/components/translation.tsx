89a6a5896f8450afdca064811cf7bac6e414dbb6
FrankFang <frankfang1990@gmail.com>
2024-05-18 15:01:31 +0800

    fix lints

--- src/components/translation.tsx
+++ src/components/translation.tsx
@@ -1,13 +1,13 @@
 import type { supportedLocales } from "~/lib/constants"
-import { useLocale } from "~/lib/hooks/use-locale"
+import { getLocale } from "~/lib/hooks/get-locale"
 
 interface Nested {
   [key: string]: Nested | string
 }
 export type I18n = Record<typeof supportedLocales[number], Nested>
-const Translation = (strings: I18n) => {
-  const { locale } = useLocale()
+export const Translation = (strings: I18n) => {
+  const locale = getLocale()
   return <span>
-
+    {locale}
   </span>
 }
