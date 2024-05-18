cd49021259c1cdeaedb553b4fe5e53c1ce3301de
FrankFang <frankfang1990@gmail.com>
2024-05-19 00:31:41 +0800

    refactor code to use clsx and add red and green button variants

--- src/app/_components/speech-to-text.tsx
+++ src/app/_components/speech-to-text.tsx
@@ -1,5 +1,7 @@
 'use client'
-import { useRef, useState } from "react"
+import { type LiveClient } from "@deepgram/sdk"
+import clsx from "clsx"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { Button } from "~/components/ui/button"
 import { openDeepgramConnection } from "~/lib/deepgram"
 import { createCatcher } from "~/lib/errors"
@@ -8,35 +10,42 @@ import { api } from "~/trpc/react"
 export const SpeechToText = () => {
   const refRecorder = useRef<MediaRecorder | null>(null)
   const refStream = useRef<MediaStream | null>(null)
-  const [status, setStatus] = useState<'idle' | 'recording'>('idle')
-  const apiKeyQuery = api.deepgram.api_key.useQuery()
-  const startRecording = async () => {
-    setStatus('recording')
+  const refConnectionToDeepgram = useRef<LiveClient | null>(null)
+  const [status, setStatus] = useState<'idle' | 'recording' | 'initial'>('initial')
+  const apiKey = api.deepgram.api_key.useQuery().data?.api_key
+  const startRecording = useCallback(async () => {
+    if (!apiKey) return
     const stream = refStream.current = await navigator.mediaDevices.getUserMedia({ video: false, audio: true }).catch(createCatcher(() => {
       window.alert('If you want to use this feature, you need to allow access to your microphone.')
     }))
-    const mediaRecorder = refRecorder.current = new MediaRecorder(stream);
-    const apiKey = apiKeyQuery.data?.api_key
-    if (!apiKey) return
-    const connection = await openDeepgramConnection(apiKey)
-    mediaRecorder.ondataavailable = (e) => connection.send(e.data)
-    mediaRecorder.start(250)
-  }
-  const stopRecording = () => {
+    refRecorder.current = new MediaRecorder(stream);
+    refConnectionToDeepgram.current = await openDeepgramConnection(apiKey)
+    refRecorder.current.ondataavailable = (e) => refConnectionToDeepgram.current?.send(e.data)
+    refRecorder.current.start(250)
+  }, [apiKey])
+  const stopRecording = useCallback(() => {
     setStatus('idle')
     refStream.current?.getTracks().forEach((track) => track.stop())
     refRecorder.current?.stop()
-  }
-  const onClick = async () => {
+    refConnectionToDeepgram.current?.finish()
+  }, [])
+  useEffect(() => {
     if (status === 'recording') {
-      stopRecording()
-    } else if (status === 'idle') {
       startRecording().catch(createCatcher(console.error))
+    } else if (status === 'idle') {
+      stopRecording()
+    }
+  }, [status, startRecording, stopRecording])
+  const onClick = async () => {
+    if (['initial', 'idle'].includes(status)) {
+      setStatus('recording')
+    } else {
+      setStatus('idle')
     }
   }
   return <div className="flex items-center justify-center">
-    <Button onClick={onClick}>
-      {status === 'idle' ? 'Start Recording' : 'Stop Recording'}
+    <Button onClick={onClick} variant={status === 'recording' ? 'red' : 'green'} >
+      {status === 'recording' ? 'Stop Recording' : 'Start Recording'}
     </Button>
     <div className="fixed bottom-0 left-0 min-h-8 w-full bg-blue-300">Record</div>
   </div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        red: "bg-red-500 text-white hover:bg-red-600",
+        green: "bg-green-500 text-white hover:bg-green-600",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -35,7 +37,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
